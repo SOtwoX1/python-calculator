7c1028f5ebfef92a7d72e9a9a6b005e94aa2cd4f
SOtwoX1 <siratee6775@gmail.com>
2024-11-17 23:35:11 +0700

    FINAL

--- calculator.py
+++ calculator.py
@@ -1,25 +1,71 @@
 class Calculator:
     def add(self, a, b):
+        assert isinstance(a, int) and isinstance(b, int), "Arguments must be integers"
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        assert isinstance(a, int) and isinstance(b, int), "Arguments must be integers"
+        return a - b
 
     def multiply(self, a, b):
+        assert isinstance(a, int) and isinstance(b, int), "Arguments must be integers"
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
-        return result
+        if b < 0 and a < 0:
+            b = abs(b)
+            a = abs(a)
+            for i in range(b):
+                result = self.add(result, a)
+            return result
+        elif b < 0 and a > 0:
+            b = abs(b)
+            a = abs(a)
+            result = 0
+            for i in range(b):
+                result = self.add(result, a)
+            return -result
+        elif b > 0 and a < 0:
+            b = abs(b)
+            a = abs(a)
+            result = 0
+            for i in range(b):
+                result = self.add(result, a)
+            return -result
+        else:
+            for i in range(b):
+                result = self.add(result, a)
+            return result
 
     def divide(self, a, b):
-        result = 0
-        while a > b:
-            a = self.subtract(a, b)
-            result += 1
-        return result
+        assert isinstance(a, int) and isinstance(b, int), "Arguments must be integers"
+        assert b != 0, "Cannot divide by zero"
+        if (a < 0 or b < 0) and abs(a) > abs(b):
+            a = abs(a)
+            b = abs(b)
+            result = 0
+            while a >= b:
+                a = abs(self.subtract(a, b))
+                result += 1
+            return result
+        elif (a < 0 and b < 0) or (a > 0 and b > 0):
+            a = abs(a)
+            b = abs(b)
+            result = 0
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return result
+        else:
+            a = abs(a)
+            b = abs(b)
+            result = 0
+            while a >= b:
+                a = self.subtract(a, b)
+                result += 1
+            return -result
     
     def modulo(self, a, b):
-        while a <= b:
+        assert isinstance(a, int) and isinstance(b, int), "Arguments must be integers"
+        while a >= b:
             a = a-b
         return a
 
@@ -30,5 +76,5 @@ if __name__ == "__main__":
     print("Example: addition: ", calc.add(1, 2))
     print("Example: subtraction: ", calc.subtract(4, 2))
     print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
+    print("Example: division: ", calc.divide(8,4))
     print("Example: modulo: ", calc.modulo(10, 3))
--- test_calculator.py
+++ test_calculator.py
@@ -5,10 +5,42 @@ class TestCalculator(unittest.TestCase):
 
     def setUp(self):
         self.calc = Calculator()
-
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+    def test_add2(self):
+        self.assertEqual(self.calc.add(2, -3), -1)
+    def test_add3(self):
+        self.assertEqual(self.calc.add(-3, -5), -8)
 
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+    def test_subtract2(self):
+        self.assertEqual(self.calc.subtract(2, -3), 5)
+    def test_subtract3(self):
+        self.assertEqual(self.calc.subtract(-3, -5), 2)
+        
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+    def test_multiply2(self):
+        self.assertEqual(self.calc.multiply(2, -3), -6)
+    def test_multiply3(self):
+        self.assertEqual(self.calc.multiply(-3, -5), 15)
+        
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4, 2), 2)
+    # def test_divide2(self): # this case fails example
+    #     self.assertEqual(self.calc.divide(2, -1), -2)
+    def test_divide3(self):
+        self.assertEqual(self.calc.divide(-4, -4), 1)
+    def test_divide4(self):
+        self.assertEqual(self.calc.divide(8, 4), 2)
+    
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+    def test_modulo2(self):
+        self.assertEqual(self.calc.modulo(10, 4), 2)
+    def test_modulo3(self):
+        self.assertEqual(self.calc.modulo(10, 5), 0)
     # Add the following test methods to the TestCalculator class:
 
 if __name__ == '__main__':
